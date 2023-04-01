6ef2fcaf7480e100694f144666a9dbfcdeb4b705
johnchen383 <john.bm.chen@gmail.com>
2023-04-01 14:59:03 +1300

    feat: commented out certain code

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,75 +1,75 @@
 import { useEffect } from "react";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import * as THREE from "three";
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// import * as THREE from "three";
 import "../styles/screens/home.scss";
-import donutUrl from "../assets/donut.glb?url";
+// import donutUrl from "../assets/donut.glb?url";
 
 function Home() {
 	useEffect(() => {
-		const scene = new THREE.Scene();
-		scene.background = new THREE.Color(0x9cbec9);
+		// const scene = new THREE.Scene();
+		// scene.background = new THREE.Color(0x9cbec9);
 
-		const camera = new THREE.PerspectiveCamera(
-			50,
-			1,
-			0.1,
-			2000
-		);
-		camera.position.set(0, 0, 5);
+		// const camera = new THREE.PerspectiveCamera(
+		// 	50,
+		// 	1,
+		// 	0.1,
+		// 	2000
+		// );
+		// camera.position.set(0, 0, 5);
 
-		const renderer = new THREE.WebGLRenderer();
-		renderer.setSize(window.innerHeight * 0.7, window.innerHeight * 0.7);
-		renderer.domElement.width = window.innerHeight * 0.7;
-		renderer.domElement.height = window.innerHeight * 0.7;
+		// const renderer = new THREE.WebGLRenderer();
+		// renderer.setSize(window.innerHeight * 0.7, window.innerHeight * 0.7);
+		// renderer.domElement.width = window.innerHeight * 0.7;
+		// renderer.domElement.height = window.innerHeight * 0.7;
 
-		renderer.toneMapping = THREE.ReinhardToneMapping;
-		renderer.toneMappingExposure = 1.2;
-		renderer.shadowMap.enabled = true;
+		// renderer.toneMapping = THREE.ReinhardToneMapping;
+		// renderer.toneMappingExposure = 1.2;
+		// renderer.shadowMap.enabled = true;
 
-		const hemiLight = new THREE.HemisphereLight(0xff5555, 0xff5555, 5);
-		hemiLight.position.set(0, 0, 10);
-		scene.add(hemiLight);
+		// const hemiLight = new THREE.HemisphereLight(0xff5555, 0xff5555, 5);
+		// hemiLight.position.set(0, 0, 10);
+		// scene.add(hemiLight);
 
-		const spotlight = new THREE.SpotLight(0xff9999, 10);
-		spotlight.position.set(0, 0, 10);
-		spotlight.castShadow = true;
-		spotlight.shadow.bias = -0.001;
-		spotlight.shadow.mapSize.width = 1024 * 4;
-		spotlight.shadow.mapSize.height = 1024 * 4;
-		scene.add(spotlight);
+		// const spotlight = new THREE.SpotLight(0xff9999, 10);
+		// spotlight.position.set(0, 0, 10);
+		// spotlight.castShadow = true;
+		// spotlight.shadow.bias = -0.001;
+		// spotlight.shadow.mapSize.width = 1024 * 4;
+		// spotlight.shadow.mapSize.height = 1024 * 4;
+		// scene.add(spotlight);
 
 		//mount to DOM
-		let sceneEl = document.getElementById("scene");
-		sceneEl!.appendChild(renderer.domElement);
+		// let sceneEl = document.getElementById("scene");
+		// sceneEl!.appendChild(renderer.domElement);
 
 		//load asset
-		const gltfLoader = new GLTFLoader();
-		gltfLoader.load(donutUrl, (gltf) => {
-			const donut = gltf.scene;
-			donut.traverse(n => {
-				if (n.isObject3D) {
-					n.castShadow = true;
-					n.receiveShadow = true;
-				}
-			})
+		// const gltfLoader = new GLTFLoader();
+		// gltfLoader.load(donutUrl, (gltf) => {
+		// 	const donut = gltf.scene;
+		// 	donut.traverse(n => {
+		// 		if (n.isObject3D) {
+		// 			n.castShadow = true;
+		// 			n.receiveShadow = true;
+		// 		}
+		// 	})
 
-			donut.scale.set(5.5, 5.5, 5.5);
-			donut.rotation.set(0, 1, 1);
-			scene.add(donut);
+		// 	donut.scale.set(5.5, 5.5, 5.5);
+		// 	donut.rotation.set(0, 1, 1);
+		// 	scene.add(donut);
 
-			function animate() {
-				requestAnimationFrame(animate);
+		// 	// function animate() {
+		// 	// 	requestAnimationFrame(animate);
 
-				donut.rotation.z += 0.019;
-				donut.rotation.y += 0.02;
+		// 	// 	donut.rotation.z += 0.019;
+		// 	// 	donut.rotation.y += 0.02;
 
-				renderer.render(scene, camera);
-			}
+		// 		// renderer.render(scene, camera);
+		// 	// }
 
-			animate();
-		}, undefined, (error) => {
-			console.error(error);
-		});
+		// 	// animate();
+		// }, undefined, (error) => {
+		// 	console.error(error);
+		// });
 	}, []);
 
 	return (
